d9ffbb213c97a661dce27c9b54ac398ba08ef4ca
Jay <jevonte_n@outlook.com>
2018-10-09 12:12:55 -0400

    seeds added.

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,8 @@
-INSERT INTO burgers
+INSERT INTO burgers_db (burger_name, devoured)
+VALUES ("Bacon Double Cheeseburger", true);
+
+INSERT INTO burgers_db (burger_name, devoured)
+VALUES ("The Omega Burger", false);
+
+INSERT INTO burgers_db (burger_name, devoured)
+VALUES ("Cheeseburger", true);
